35d679dfe8db6c0590716c6338a7b4ffc5a2152a
immeguru <meguru19389@icloud.com>
2025-04-28 21:32:35 +0900

    SpringBoot1週目課題提出

--- Spring.app/src/main/java/com/yi_college/bookmanager/model/Book.java
+++ Spring.app/src/main/java/com/yi_college/bookmanager/model/Book.java
@@ -2,11 +2,13 @@ package com.yi_college.bookmanager.model;
 
 import java.time.LocalDate;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class Book {
 	
-	public Book(int id, String title, String author, String publisher, LocalDate  publicationDate, int stock) {
-		// TODO 自動生成されたコンストラクター・スタブ
-	}
 	private int id;
 	private String title;
 	private String author;
@@ -14,7 +16,61 @@ public class Book {
 	private LocalDate publicationdate;
 	private int stock;
 	
+	public Book(int id, String title, String author, String publisher, LocalDate publicationDate, int stock) {
+		// TODO 自動生成されたコンストラクター・スタブ
+//	    this.setId(id);
+//		this.setTitle(title);
+//		this.setAuthor(author);
+//		this.setPublisher(publisher);
+//		this.setPublicationdate(publicationDate);
+//		this.setStock(stock);
+		this.id = id;
+		this.title = title;
+		this.author = author;
+		this.publisher = publisher;
+		this.publicationdate = publicationDate;
+		this.stock = stock;
+		
 	}
+	
+//	public int getId() {
+//		return id;
+//	}
+//	public void setId(int id) {
+//		this.id = id;
+//	}
+//	public String getTitle() {
+//		return title;
+//	}
+//	public void setTitle(String title) {
+//		this.title = title;
+//	}
+//	public LocalDate getPublicationdate() {
+//		return publicationdate;
+//	}
+//	public void setPublicationdate(LocalDate publicationdate) {
+//		this.publicationdate = publicationdate;
+//	}
+//	public String getPublisher() {
+//		return publisher;
+//	}
+//	public void setPublisher(String publisher) {
+//		this.publisher = publisher;
+//	}
+//	public int getStock() {
+//		return stock;
+//	}
+//	public void setStock(int stock) {
+//		this.stock = stock;
+//	}
+//	public String getAuthor() {
+//		return author;
+//	}
+//	public void setAuthor(String author) {
+//		this.author = author;
+//	}
+	
+}
 
 
 
--- Spring.app/src/main/java/com/yi_college/bookmanager/service/BookService.java
+++ Spring.app/src/main/java/com/yi_college/bookmanager/service/BookService.java
@@ -12,23 +12,19 @@ import com.yi_college.bookmanager.model.Book;
 @Service
 public class BookService {
 	
-	private final List<Book> books;
 	
-    public BookService() {
-       books = Arrays.asList(
-    		   
-    		new Book(1,"ハリーポッター","J.K.ローリング","株式会社静山社",LocalDate.of(1999,12, 1),7),
-    		new Book(2,"星の王子様","アントワーヌ.ド.サン=テグジュペリ","新潮社",LocalDate.of(2006,3,28),3),
-    		new Book(3,"人間失格","太宰治","新潮社",LocalDate.of(2006,1,1),4),
-    		new Book(4,"吾輩は猫である","夏目漱石","文藝春秋",LocalDate.of(2011,11,10),4),
-    		new Book(5,"たけくらべ","樋口一葉","集英社",LocalDate.of(1993,12,15),5)
-    				
-    		   );
 	
-    }
+    
     public List<Book> getBooks(){
-    	return books;
-    	
+    	return  Arrays.asList(
+     		   
+        		new Book(1,"ハリーポッター","J.K.ローリング","株式会社静山社",LocalDate.of(1999,12, 1),7),
+        		new Book(2,"星の王子様","アントワーヌ.ド.サン=テグジュペリ","新潮社",LocalDate.of(2006,3,28),3),
+        		new Book(3,"人間失格","太宰治","新潮社",LocalDate.of(2006,1,1),4),
+        		new Book(4,"吾輩は猫である","夏目漱石","文藝春秋",LocalDate.of(2011,11,10),4),
+        		new Book(5,"たけくらべ","樋口一葉","集英社",LocalDate.of(1993,12,15),5)
+        				
+        		   );
 	
 		
 		
